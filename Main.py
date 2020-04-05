d4c66132fcac88c4b8b756d42f3e29560a2cdfbf
officiallyaninja <officiallyaninja@gmail.com>
2020-04-05 13:27:43 +0530

    Added documentation
    i added comments to all of govind's code

--- Main.py
+++ Main.py
@@ -1,51 +1,62 @@
+''''
+it is reccomended that you read this program from bottom to top
+'''
 import sys
+# for exiting the program
 import json
+# for loading and storing save files
 import random
+# used in jmap
 
 guess_count = n = num = op = Jnum = Jop = Jnum_check = Jop_check = 1  # Initialising Global Variables
 
 
-def Jmap(licht):  # Jumbled Mapper
+def Jmap(licht):  # Jumbled Mapper.
 
-    Jumbled = random.sample(licht, len(licht))
+    Jumbled_licht = random.sample(licht, len(licht))  # comletely randomizes the list
 
     Mapping = {}
 
-    for x in range(0, len(licht)):
-        Mapping[licht[x]] = Jumbled[x]
+    for i in range(0, len(licht)):
+        Mapping[licht[i]] = Jumbled_licht[i]
 
-    return(Mapping)
+    return Mapping
+
+# main game function, just call game() to run the game
 
 
-def Game(x):
+def Game(x):  # x is true if its a new game, false if its been loaded
 
-    global guess_count
-    global n
-    global num
-    global op
-    global Jnum
-    global Jop
-    global Jnum_check
-    global Jop_check
+    # need global keyword so it uses the previously initialised global variables and doesnt create new local variables
+    global guess_count  # counts how many consecutive chances the player has left to guess
+    global n  # sets size of jumble, numbers from 0 to n are jumbled in the game
+    global num  # list of all the numbers to be jumbled
+    global op  # list of all valid operations
+    global Jnum  # a dictionary that maps the jumbled numbers
+    global Jop  # dictionary that maps the jumbled operations
+    global Jnum_check  # list of all numbers player has left to correctly guess
+    global Jop_check  # list of all operations player has left to correctly guess
 
-    if x is True:
+    if x is True:  # for new game
 
         n = int(input("Enter range limit : "))
         num = list(range(0, n + 1))
 
-        op = ["+", "-", "*", "/"]  # op means operation
+        op = ["+", "-", "*", "/"]
 
+        # initializes the dictionaries
         Jnum = Jmap(num)
         Jop = Jmap(op)
 
-        Jnum_check = list(Jnum.keys())  # Check to see remaining keys to find
+        Jnum_check = list(Jnum.keys())
         Jop_check = list(Jop.keys())
 
         guess_count = 3  # setting up for the initial guess
 
-    if x is False:
+    if x is False:  # for loaded game
 
         with open('Save.json') as json_file:
+            # save is the dictionary with all the saved data
             save = json.load(json_file)
             n = save['n']
             num = save['num']
@@ -68,14 +79,14 @@ def Game(x):
 
     def Save():
 
-        global guess_count
-        global n
-        global num
-        global op
-        global Jnum
-        global Jop
-        global Jnum_check
-        global Jop_check
+        global guess_count  # counts how many consecutive chances the player has left to guess
+        global n  # sets size of jumble, numbers from 0 to n are jumbled in the game
+        global num  # list of all the numbers to be jumbled
+        global op  # list of all valid operations
+        global Jnum  # a dictionary that maps the jumbled numbers
+        global Jop  # dictionary that maps the jumbled operations
+        global Jnum_check  # list of all numbers player has left to correctly guess
+        global Jop_check  # list of all operations player has left to correctly guess
 
         save = {}
         save['n'] = n
@@ -111,17 +122,23 @@ def Game(x):
         global Jnum_check
         global Jop_check
 
-        K = input("Enter Number / Operation (Jumbled) : ")
-        V = input("Enter Guess : ")
+        K = input("Enter Number / Operation (Jumbled) : ")  # K is the symbol
+        V = input("Enter Guess : ")                         # V is the value that symbol holds
 
+        # x only gives true for correctly guessed operations; x is the key-value check for operations
         if K in Jop.keys():
-            x = (V == Jop[K])
+            x = (V == Jop[K])  # we need x as a dummy variable in case Jop[K] gives KeyError.
         else:
             x = False
 
-        if x or (V == str(Jnum[int(K)])):
+        # y is true if player correctly guesses jumbled number; y is the key-value check for number
+        y = (V == str(Jnum[int(K)]))
+
+        if x or y:
             print("Your Guess was right !")
-            guess_count = 3
+            guess_count = 3  # resets chances for guessing
+
+            # removes the correctly guessed number or operations from checklists(Jnum_check and Jop_check)
             if K in list(map(str, Jnum_check)):
                 Jnum_check.remove(int(K))
             elif K in Jop_check:
@@ -129,7 +146,7 @@ def Game(x):
             else:
                 pass
 
-            if len(Jop_check) == len(Jnum_check) == 0:
+            if len(Jop_check) == len(Jnum_check) == 0:  # if there is nothing left to guess
                 print("You Won !")
                 sys.exit()
 
